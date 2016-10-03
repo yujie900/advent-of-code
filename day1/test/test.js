3e75a34c218ee24110fae1257a72575fffa5ab4b
Yujie Wu <ywu24@uh.edu>
2016-10-03 12:55:16 -0500

    restructured dir again, changed me mind lads

--- day1/test/test.js
+++ day1/test/test.js
@@ -1,6 +1,5 @@
 const should = require('should');
 const day1 = require('../day1.js');
-const day2 = require('../day2.js');
 const fs = require('fs');
 
 describe('day1', function () {
@@ -29,30 +28,3 @@ describe('day1', function () {
     });
   });
 });
-
-describe("day2", function() {
-  var day2_input;
-  fs.readFile("day2_input.txt", "utf8", (err, data) => {
-    if (err)
-    { throw err;}
-    day2_input = data;
-  });
-
-  describe("findTotal", function () {
-    it("should return 58 for 2x3x4", function () {
-      day2.findTotal("2x3x4").should.be.exactly(58);
-    });
-    it("should return 43 for 1x1x10", function () {
-      day2.findTotal("1x1x10").should.be.exactly(43);
-    });
-    it("should return 101 for 2x3x4\n1x1x10", function () {
-      day2.findTotal("2x3x4\n1x1x10").should.be.exactly(101);
-    });
-    it("should return correct value for input", function () {
-      day2.findTotal(day2_input).should.be.exactly(1606483);
-    });
-  });
-
-});
-
-
